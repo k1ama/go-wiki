6cd1d18ca060058e66a00f7af75742ecc73ba85f
k1ama <ianpeterkiama@gmail.com>
2024-08-08 20:45:40 +0300

    feat: add save page functionality

--- wiki.go
+++ wiki.go
@@ -2,7 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
 
 func main() {
 	fmt.Println("Welcome To Wiki")
